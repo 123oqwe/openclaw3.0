b10d64b87acf9ad131e8592b8a0d376a281a1b92
Harry Qiao <254048057+123oqwe@users.noreply.github.com>
2026-09-10 18:10:06 -0400

    fix(outcomes): format public contract test

--- packages/outcomes-contract/src/index.test.ts
+++ packages/outcomes-contract/src/index.test.ts
@@ -21,18 +21,18 @@ describe("outcomes public contract", () => {
 
   it("keeps core detail views free of persistence identity and internal history", () => {
     expect(Object.keys(contract).sort()).toEqual([
-        "OUTCOME_ACCEPTANCE_VALIDITY",
-        "OUTCOME_ATTENTION_CODES",
-        "OUTCOME_DEFAULT_LIST_LIMIT",
-        "OUTCOME_EVIDENCE_KINDS",
-        "OUTCOME_MAX_LIST_LIMIT",
-        "OUTCOME_PROJECTION_MAX_AGE_MS",
-        "OUTCOME_NEXT_ACTIONS",
-        "OUTCOME_PHASES",
-        "OUTCOME_PROOF_STATUSES",
-        "OUTCOME_READINESS",
-        "OUTCOME_SOURCE_ISSUE_REASONS",
-        "OUTCOME_SOURCE_VISIBILITY",
-      ].sort());
+      "OUTCOME_ACCEPTANCE_VALIDITY",
+      "OUTCOME_ATTENTION_CODES",
+      "OUTCOME_DEFAULT_LIST_LIMIT",
+      "OUTCOME_EVIDENCE_KINDS",
+      "OUTCOME_MAX_LIST_LIMIT",
+      "OUTCOME_NEXT_ACTIONS",
+      "OUTCOME_PHASES",
+      "OUTCOME_PROOF_STATUSES",
+      "OUTCOME_PROJECTION_MAX_AGE_MS",
+      "OUTCOME_READINESS",
+      "OUTCOME_SOURCE_ISSUE_REASONS",
+      "OUTCOME_SOURCE_VISIBILITY",
+    ].sort());
   });
 });
