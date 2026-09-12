3fc24723a7575eaa44e3f20e14b706883ad1c0f5
Harry Qiao <254048057+123oqwe@users.noreply.github.com>
2026-09-12 04:01:42 -0400

    style(outcomes): apply hosted gateway format patch

--- extensions/outcomes/src/gateway/methods.ts
+++ extensions/outcomes/src/gateway/methods.ts
@@ -26,11 +26,7 @@ import {
   normalizedUuid,
   withRefs,
 } from "./input-normalizers.js";
-import {
-  fail,
-  reportCapacityWarning,
-  respondMutation,
-} from "./method-helpers.js";
+import { fail, reportCapacityWarning, respondMutation } from "./method-helpers.js";
 import {
   outcomeCancelParamsSchema,
   outcomeActivateParamsSchema,
